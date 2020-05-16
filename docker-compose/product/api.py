32886a6a8ab228b69c2e507f1de783c10259ef63
Lewis Dukelow <dukelowlewis@gmail.com>
2020-05-16 02:10:00 +0100

    💻Added php website

--- docker-compose/product/api.py
+++ docker-compose/product/api.py
@@ -7,10 +7,11 @@ api = Api(app)
 class Product(Resource):
     def get(self):
         return {
-            'product': [
+            'products': [
                 'Ice cream',
                 'Chocolate',
-                'Fruit'
+                'Fruit',
+                'eggs'
                 ]
         }
 
